c4449a08773bb3533b196f13ffa95ce10802775a
Roman Yasinovskyy <yasinovskyy@gmail.com>
2023-12-04 10:58:56 -0600

    Add query demo

--- src/main/java/simpledb/query/SelectScanDemo.java
+++ src/main/java/simpledb/query/SelectScanDemo.java
@@ -1,37 +1,36 @@
 package simpledb.query;
 
 import simpledb.metadata.MetadataMgr;
-import simpledb.plan.JoinPlan;
+import simpledb.plan.SelectPlan;
 import simpledb.plan.Plan;
 import simpledb.plan.TablePlan;
 import simpledb.server.SimpleDB;
 import simpledb.tx.Transaction;
 
 /**
- * Demo of the scan class corresponding to the <i>join</i> relational
+ * Demo of the scan class corresponding to the <i>select</i> relational
  * algebra operator.
  * 
  * @author roman
  */
-public class JoinScanDemo {
+public class SelectScanDemo {
     public static void main(String[] args) {
-        System.out.println("JOIN");
         SimpleDB db = new SimpleDB("collegedb");
         MetadataMgr mdm = db.mdMgr();
+        System.out.println("SELECT");
         Transaction tx = db.newTx();
         Plan studentTblPlan = new TablePlan(tx, "student", mdm);
-        Plan deptTblPlan = new TablePlan(tx, "dept", mdm);
         tx.commit();
-        Plan plan = new JoinPlan(studentTblPlan, deptTblPlan,
+        Plan plan = new SelectPlan(studentTblPlan,
                 new Predicate(
                         new Term(
                                 new Expression("majorid"),
-                                new Expression("did"))));
+                                new Expression(new Constant(10)))));
+        Scan scan = plan.open();
         for (String field : plan.schema().fields()) {
             System.out.printf("%10s", field);
         }
         System.out.println();
-        Scan scan = plan.open();
         while (scan.next()) {
             for (String field : plan.schema().fields()) {
                 System.out.printf("%10s", scan.getVal(field).toString());
